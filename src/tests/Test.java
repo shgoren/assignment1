172ff14d5ca091e65d0d239eb88df7566f57245b
shgoren <34242458+shgoren@users.noreply.github.com>
2017-12-06 14:29:40 +0200

    sdsd

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -15,14 +15,11 @@ public class Test {
 		WAVLNode node2 = tree2.new WAVLNode(2,"two");
 		
 		tree2.test();
-<<<<<<< HEAD
 		System.out.println(tree2.min());
 		System.out.println(tree2.max());
 		System.out.println(tree2.search(10));
 		System.out.print(Arrays.toString(tree2.keysToArray()));
 		System.out.print(tree2.root.leftSon.rightSon.getKey());
-		
->>>>>>> master
 		
 		
 
