2c9e37dc48970fbb94d92667e79495cbd82645c7
shgoren <34242458+shgoren@users.noreply.github.com>
2017-12-11 17:17:27 +0200

    sds

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -14,34 +14,24 @@ public class Test {
 				tree2.insert(i, ""+i);
 			}
 			for (int i = 8; i<=15; i++) {
-				if(i==10 && j==16)
-					TreePrinter.printNode(tree2.root);
 				tree2.delete(i);
-				for(int k = 1; k<=j; k++) {
-					node = tree2.root.searchNode(k);
-					if(node.isRealNode()) {
-						if(!(node.treeSize == (node.leftSon.treeSize + node.rightSon.treeSize +1)))
-							System.out.println("size error in tree of size: "+j+" in node: "+node.getKey()+" i = "+i);
-					}
-				}
-				
 			}
-			/*for (int i = 1; i<=8; i++) {
+			for (int i = 1; i<=8; i++) {
 				tree2.delete(i);
 			}
 			for (int i = 1; i<=j; i++) {
 				tree2.insert(i, ""+i);
 			}
 
-			node = tree2.root.searchNode(i);
 			for (int i = 1; i<=40; i++) {
+				node = tree2.root.searchNode(i);
 				if(node.isRealNode()) {
 					if(!(node.treeSize == (node.leftSon.treeSize + node.rightSon.treeSize +1)))
 						System.out.println("size error in tree of size: "+j+" in node: "+node.getKey());
 					if(!node.isValidDifs())
 						System.out.println("rank error in tree of size: "+j+" in node: "+node.getKey());
 				}
-			}*/
+			}
 		}
 		
 		
