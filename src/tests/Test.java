c52efe62d24fdc694a2b9c75b3765a0fbb8fbbc2
shgoren <34242458+shgoren@users.noreply.github.com>
2017-12-11 18:14:17 +0200

    analysis test

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -6,34 +6,34 @@ import wavl.WAVLTree.WAVLNode;
 public class Test {
 	
 	public static void main(String[] args) {
-
-		WAVLNode node;
+		int[] insertCounter = new int[10],
+			  deleteCounter = new int[10];
+		int max = 0,
+			currOps = 0;
 		WAVLTree tree2 = new WAVLTree();
-		for(int j=1; j<40; j++) {
-			for (int i = 1; i<=j; i++) {
-				tree2.insert(i, ""+i);
+		for(int i=1; i<=10; i++) {
+			for(int j=1; j<=i*10000;j++) {
+				currOps = tree2.insert(j,""+j);
+				insertCounter[i-1] += currOps;
+				if (currOps>max)
+					max = currOps;
 			}
-			for (int i = 8; i<=15; i++) {
-				tree2.delete(i);
-			}
-			for (int i = 1; i<=8; i++) {
-				tree2.delete(i);
-			}
-			for (int i = 1; i<=j; i++) {
-				tree2.insert(i, ""+i);
-			}
-
-			for (int i = 1; i<=40; i++) {
-				node = tree2.root.searchNode(i);
-				if(node.isRealNode()) {
-					if(!(node.treeSize == (node.leftSon.treeSize + node.rightSon.treeSize +1)))
-						System.out.println("size error in tree of size: "+j+" in node: "+node.getKey());
-					if(!node.isValidDifs())
-						System.out.println("rank error in tree of size: "+j+" in node: "+node.getKey());
-				}
+			for(int j=1; j<=i*10000;j++) {
+				currOps = tree2.delete(j);
+				deleteCounter[i-1] += currOps;
+				if (currOps>max)
+					max = currOps;
 			}
 		}
-		
+		System.out.println("max number of rebalance operations:" + max);
+		System.out.println("total operations of rebalances for inserts in experiments:");
+		for(int i=0; i<10; i++)
+			System.out.println("     experiment number: "+(i+1)+":"+insertCounter[i]+
+					"\t\n          average is: "+insertCounter[i]/((i+1)*10000.0));
+		System.out.println("total operations of rebalances for deletions in experiments:");
+		for(int i=0; i<10; i++)
+			System.out.println("     experiment number: "+(i+1)+":"+deleteCounter[i]+
+					"\t\n          average is: "+(deleteCounter[i]/((i+1)*10000.0)));
 		
 		
 
