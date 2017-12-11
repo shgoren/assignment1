fe1f917ea21f198f22e4d5e245b25f3f422fca2f
shgoren <34242458+shgoren@users.noreply.github.com>
2017-12-11 19:53:57 +0200

    test fix

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -8,28 +8,33 @@ public class Test {
 	public static void main(String[] args) {
 		int[] insertCounter = new int[10],
 			  deleteCounter = new int[10];
-		int max = 0,
+		int maxInsert = 0,
+			maxDelete = 0,
 			currOps = 0;
 		WAVLTree tree2 = new WAVLTree();
 		for(int i=1; i<=10; i++) {
 			for(int j=1; j<=i*10000;j++) {
 				currOps = tree2.insert(j,""+j);
 				insertCounter[i-1] += currOps;
-				if (currOps>max)
-					max = currOps;
+				if (currOps>maxInsert)
+					maxInsert = currOps;
 			}
 			for(int j=1; j<=i*10000;j++) {
 				currOps = tree2.delete(j);
 				deleteCounter[i-1] += currOps;
-				if (currOps>max)
-					max = currOps;
+				if (currOps>maxDelete)
+					maxDelete = currOps;
 			}
 		}
-		System.out.println("max number of rebalance operations:" + max);
+		System.out.println("max number of insert rebalance operations:" + maxInsert);
 		System.out.println("total operations of rebalances for inserts in experiments:");
 		for(int i=0; i<10; i++)
 			System.out.println("     experiment number: "+(i+1)+":"+insertCounter[i]+
 					"\t\n          average is: "+insertCounter[i]/((i+1)*10000.0));
+		
+		System.out.println("\n");
+		
+		System.out.println("max number of insert rebalance operations:" + maxDelete);
 		System.out.println("total operations of rebalances for deletions in experiments:");
 		for(int i=0; i<10; i++)
 			System.out.println("     experiment number: "+(i+1)+":"+deleteCounter[i]+
